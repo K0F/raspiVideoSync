918c33b6d777ffad1361254dfc8b04821b0a827a
k0f <krystof.pesek@gmail.com>
2016-05-09 14:55:29 +0200

    simple detection

--- send.sh
+++ send.sh
@@ -2,8 +2,11 @@
 #source /home/pi/.bashrc
 export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin/
 
-(sleep 1s && echo -n p | /usr/bin/socat - UDP4-DATAGRAM:239.1.1.250:7777,so-broadcast)&
-(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:239.1.1.250:7777,so-broadcast)&
-(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:239.1.1.250:7777,so-broadcast)&
-(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:239.1.1.250:7777,so-broadcast)&
-(sleep 3s && echo -n p | /usr/bin/socat - UDP4-DATAGRAM:239.1.1.250:7777,so-broadcast)&
+$BR=`sudo ifconfig | grep broadcast | awk '{print $6}'`
+(echo broadcast: $BR >> /home/pi/log.txt)&
+
+(sleep 1s && echo -n p | /usr/bin/socat - UDP4-DATAGRAM:$BR:7777,so-broadcast)&
+(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:$BR:7777,so-broadcast)&
+(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:$BR:7777,so-broadcast)&
+(sleep 2s && echo -n i | /usr/bin/socat - UDP4-DATAGRAM:$BR:7777,so-broadcast)&
+(sleep 3s && echo -n p | /usr/bin/socat - UDP4-DATAGRAM:$BR:7777,so-broadcast)&
