b2c0d88237ebdb3f132a85b00a7fb8ff51afec58
k0f <krystof.pesek@gmail.com>
2016-05-09 15:25:12 +0200

    sound

--- run.sh
+++ run.sh
@@ -12,47 +12,11 @@ sleep 10s
 
 ## MAKE CONTROL FIFO
 (mkfifo /tmp/ctl)&
-
-ip2int()
-{
-    local a b c d
-    { IFS=. read a b c d; } <<< $1
-    echo $(((((((a << 8) | b) << 8) | c) << 8) | d))
-}
-
-int2ip()
-{
-    local ui32=$1; shift
-    local ip n
-    for n in 1 2 3 4; do
-        ip=$((ui32 & 0xff))${ip:+.}$ip
-        ui32=$((ui32 >> 8))
-    done
-    echo $ip
-}
-
-netmask()
-{
-    local mask=$((0xffffffff << (32 - $1))); shift
-    int2ip $mask
-}
-
-
-broadcast()
-{
-    local addr=$(ip2int $1); shift
-    local mask=$((0xffffffff << (32 -$1))); shift
-    int2ip $((addr | ~mask))
-}
-
-
-IP=sudo ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-
 ## ONE SHOT VARIANT
 ## LISTEN FOR SYNC
 (while true; do ((/bin/nc -luw0 7777) >> /tmp/ctl) ; done)&
 ## PLAY ONCE
-(omxplayer -b -o local --no-osd /home/pi/raspiVideoSync/checker.mp4 < /tmp/ctl)&
+(omxplayer -b --loop --no-osd /home/pi/raspiVideoSync/checker.mp4 < /tmp/ctl)&
 ## SENDER ONLY
 (sleep 30s && /home/pi/raspiVideoSync/send.sh )&
 
